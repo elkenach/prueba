15fa79152c61de610c92f01504dcdf3939b03f4c
Elena <elenachavez36@gmail.com>
2020-04-16 15:33:41 -0500

    nav

--- js/navbar.js
+++ js/navbar.js
@@ -1,70 +1,84 @@
 
+
 let nav = document.getElementById('nav');
 let menu = document.getElementById('enlaces');
-let open = document.getElementById('open');
-let buttons = document.getElementsByClassName('btn-header');
-let close = true;
-
+let abrir = document.getElementById('open');
+let botones = document.getElementsByClassName('btn-header');
+let cerrado = true;
 
 function menus(){
-	let Desplazamiento_Actual = window.pageYOffset;
+    let Desplazamiento_Actual = window.pageYOffset;
 
-	if (Desplazamiento_Actual <= 380) {
-		nav.classList.remove("nav2");
-		nav.className = ("nav1");	
-		nav.style.transition = ".8s";
-		menu.style.top = "60px";
-		open.style.color = "white" ;
-	}else{
-		nav.classList.remove("nav1");
-		nav.className = ("nav2");
-		nav.style.transition = ".8s";
-		menu.style.top = "70px";
-		open.style.color = "black" ;
-	}
-}
-
-
-function opening(){
-    if(close){
-        menu.style.width = '45vw';
-        close = false;
+    if(Desplazamiento_Actual <= 300){
+        nav.classList.remove('nav2');
+        nav.className = ('nav1');
+        nav.style.transition = '1s';
+        menu.style.top = '80px';
+        abrir.style.color = '#fff';
     }else{
-        menu.style.width = '0%';
-        menu.style.overflow = 'hidden';
-        close = true;
+        nav.classList.remove('nav1');
+        nav.className = ('nav2');
+        nav.style.transition = '1s';
+        menu.style.top = '100px';
+        abrir.style.color = '#000';
     }
 }
 
-window.addEventListener("load", function(){
-	menus();
+function apertura(){
+    if(cerrado){
+        menu.style.width = '70vw';
+        cerrado = false;
+    }else{
+        menu.style.width = '0%';
+        menu.style.overflow = 'hidden';
+        cerrado = true;
+    }
+}
+
+window.addEventListener('load', function(){
+    $('#onload').fadeOut();
+    $('body').removeClass('hidden');
+    menus();
+});
+window.addEventListener('click',function(e){
+    console.log(e.target);
+    if(cerrado==false){
+        let span = document.querySelector('span');
+        if(e.target !== span && e.target !== abrir){
+            menu.style.width = '0%';
+            menu.style.overflow = 'hidden';
+            cerrado = true;
+        }
+    }
+});
+window.addEventListener('scroll', function(){
+    console.log(window.pageYOffset);
+    menus();
+});
+window.addEventListener('resize', function(){
+    if(screen.width>= 700){
+        cerrado = true;
+        menu.style.removeProperty('overflow');
+        menu.style.removeProperty('width');
+    }
+});
+abrir.addEventListener('click',function(){
+    apertura();
 });
 
-window.addEventListener("click", function(e){
-	if (close == false) {
-		let span = document.querySelector("span");
-		if (e.target !== span && e.target !== open) {
-			menu.style.width = "0%";
-			menu.style.overflow = "hidden";
-			close = true;
-		}
-	}
-});
 
-window.addEventListener("scroll", function(){
-	console.log(window.pageYOffset);
 
-	menus(); 
-});
+/* Codigo para filtrar Deportes
 
-window.addEventListener("resize", function(){
-	if (screen.width >= 700) {
-		close = true;
-		menu.style.removeProperty ("overflow");
-		menu.style.removeProperty ("width");
-	}
-});
-
-open.addEventListener("click", function(){
-	opening();
-});
+$(function () {
+    $(".filter").click(function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        let valor =  $(this).attr("data-nombre");
+        if(valor == "todos"){
+            $(".content-post").show("1000");    
+        }else{
+            $(".content-post").not("."+ valor).hide("1000");
+            $(".content-post").filter("."+ valor).show("1000");
+        }        
+    });
+});*/
